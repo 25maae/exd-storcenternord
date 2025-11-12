d278f672e750bfd55a4dcad8e841699dcfd028a7
25maae <eaa25maae@students.eaaa.dk>
2025-11-12 13:33:59 +0100

    hele-siden

---
+++ exd-akvarie/js/animate-objects-on-road.js
@@ -0,0 +1,93 @@
+"use strict";
+
+// funktion der afspiller lyd når boble klikkes og tilføjer pop animation
+function handleBubbleClick(event) {
+  const bubbleSound = new Audio("sound/bubble-pop.mp3"); //ændrer lydfil til boblelyd
+  bubbleSound.play();
+}
+
+// venter med at hente js siden tilden hele html er loaded
+document.addEventListener("DOMContentLoaded", () => {
+  // Add click handlers to all bubbles
+  document.querySelectorAll(".bubble").forEach((bubble) => {
+    bubble.addEventListener("click", handleBubbleClick);
+  });
+});
+
+// lav den nu med skattekisten, når man hover over den så skal der afspilles en lyd af skattekisten der åbnes, med skattekiste-sounden
+function handleSkattekisteHover(event) {
+  const skattekisteSound = new Audio("sound/skattekiste-sound.mp3");
+  skattekisteSound.play();
+}
+
+// venter med at hente js siden tilden hele html er loaded
+document.addEventListener("DOMContentLoaded", () => {
+  // Add hover handler to skattekiste
+  const skattekiste = document.getElementById("skattekiste");
+  skattekiste.addEventListener("mouseover", handleSkattekisteHover);
+});
+
+function playBtnClick(event) {
+  const playBtnSound = new Audio("sound/eating-chips.mp3");
+  playBtnSound.play();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const playBtn = document.getElementById("play-button");
+  playBtn.addEventListener("click", playBtnClick);
+});
+
+function piratClick(event) {
+  const piratSound = new Audio("sound/wooden-ship-break.mp3");
+  piratSound.play();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const piratskib = document.getElementById("piratskib");
+  piratskib.addEventListener("click", piratClick);
+});
+
+function faktaBtn(event) {
+  const piratSound = new Audio("sound/wooden-ship-break.mp3");
+  piratSound.play();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const piratskib = document.getElementById("piratskib");
+  piratskib.addEventListener("click", piratClick);
+});
+
+// Tilføj event listener til knappen for at navigere til spil siden
+document.getElementById("play-button").addEventListener("click", function () {
+  window.location.href = "../exd-spil/index.html";
+});
+
+// Tilføj event listener til knappen for at navigere til fakta siden
+document.getElementById("fish-button").addEventListener("click", function () {
+  window.location.href = "../exd-facts/facts-side.html";
+});
+
+const fishInfo = [
+  {
+    fishName: "Oliver",
+    fishColor: "blue",
+    fishAge: 4
+  },
+  {
+    fishName: "Tenna",
+    fishColor: "yellow",
+    fishAge: 5
+  },
+  {
+    fishName: "Martin",
+    fishColor: "green",
+    fishAge: 6
+  },
+  {
+    fishName: "Freja",
+    fishColor: "orange og hvid",
+    fishAge: 4
+  }
+];
+
+
---
+++ exd-facts/js/animate-objects-on-road.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const fishInfo = [
+  {
+    fishName: "Gul kirurgfisk",
+    fishColor: "Gul",
+    fishLocation: "Det centrale/vestlige Stillehav",
+    fishLength: "Op til 20 cm",
+    fishFood: "Alger og små krebsdyr",
+  },
+  {
+    fishName: "Dværgkejserfisk",
+    fishColor: "Orange og sort",
+    fishLocation:
+      "Australien, Indonesien, Det østlige Stillehav og Det centrale/vestlige Stillehav",
+    fishLength: "Op til 19 cm",
+    fishFood: "Alger og små krebsdyr",
+  },
+  {
+    fishName: "Paletkirurgfisk",
+    fishColor: "Blå, sort og gul",
+    fishLocation: "Det Indiske Ocean og det vestlige Stillehav",
+    fishLength: "Op til 31 cm",
+    fishFood: "Alger og små krebsdyr",
+  },
+  {
+    fishName: "Klovnefisk",
+    fishColor: "Orange, sort og hvid",
+    fishLocation: "Det østlige Indiske Ocean, Australien og Indonesien",
+    fishLength: "Op til 8 cm",
+    fishFood: "Mikroalger, zooplankton og små krebsdyr",
+  },
+  {
+    fishName: "Pudsefisk",
+    fishColor: "Hvid, blå og sort",
+    fishLocation:
+      "Det østlige Indiske Ocean, Det vestlige Indiske Ocean, Australien, Det Røde Hav, Indonesien, Det østlige Stillehav og Det centrale/vestlige Stillehav",
+    fishLength: "Op til 11 cm",
+    fishFood: "Parasitter, små krebsdyr og zooplankton",
+  },
+  {
+    fishName: "Hvidstrubet kirugfisk",
+    fishColor: "Hvid, blå, gul og sort",
+    fishLocation: "Det østlige Indiske Ocean og Indonesien",
+    fishLength: "Op til 30 cm",
+    fishFood: "Alger og små krebsdyr",
+  },
+  {
+    fishName: "Pincetfisk",
+    fishColor: "Hvid, gul, orange og sort",
+    fishLocation: "Det vestlige Indiske Ocean, Australien og Indonesien",
+    fishLength: "Op til 20 cm",
+    fishFood: "Hvirvelløse dyr, små krebsdyr, større krebsdyr og zooplankton",
+  },
+  {
+    fishName: "Sortbåndet kirugfisk",
+    fishColor: "Hvid og sort",
+    fishLocation: "Det øst og vestlige Indiske Ocean",
+    fishLength: "Op til 11 cm",
+    fishFood: "Alger",
+  },
+];
